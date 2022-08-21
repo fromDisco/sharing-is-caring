f513242f00c4d4458eec89ad87cb61274a726897
Michel Holzky <doc.snuggles@web.de>
2022-08-21 19:20:18 +0200

    best program

---
+++ best-program-in-the-world.py
@@ -0,0 +1,9 @@
+def best_program_in_the_world(arg, nums):
+    for num in nums:
+        print(arg[num], end="")
+
+
+nums = [8, 26, 11, 14, 21, 4, 26, 21, 8, 12]
+alpha = "abcdefghijklmnopqrstuvwxyz "
+
+best_program_in_the_world(alpha, nums)
