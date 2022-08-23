f4e39af47714872bf8348a71c84eeda66a118dee
dragon-fire-fly <ek93@hotmail.co.uk>
2022-08-23 17:26:26 +0200

    Emily's changes

--- best-program-in-the-world.py
+++ best-program-in-the-world.py
@@ -1,9 +1,12 @@
 def best_program_in_the_world(arg, nums):
     for num in nums:
         print(arg[num], end="")
+    print("")    
 
 
-nums = [8, 26, 11, 14, 21, 4, 26, 21, 8, 12]
+nums = [8, 26, 11, 14, 21, 4, 26, 4, 12, 8, 11, 24]
 alpha = "abcdefghijklmnopqrstuvwxyz "
 
 best_program_in_the_world(alpha, nums)
+
+# It's better now :D 
