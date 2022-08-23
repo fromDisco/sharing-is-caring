9a734542ecc4036b0338a0467c098367f19878ea
peerhoffmanncode <peerhoffmann81@gmail.com>
2022-08-23 17:03:39 +0200

    peer change

--- best-program-in-the-world.py
+++ best-program-in-the-world.py
@@ -2,8 +2,10 @@ def best_program_in_the_world(arg, nums):
     for num in nums:
         print(arg[num], end="")
 
+# this are the changes i did (peer)
 
 nums = [8, 26, 11, 14, 21, 4, 26, 21, 8, 12]
 alpha = "abcdefghijklmnopqrstuvwxyz "
 
 best_program_in_the_world(alpha, nums)
+# this are the changes i did (peer)
