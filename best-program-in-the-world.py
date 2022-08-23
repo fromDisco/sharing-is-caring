c9c8a95ea69a3b019701e05bf543a78bc50aa8ad
peerhoffmanncode <peerhoffmann81@gmail.com>
2022-08-23 18:07:18 +0200

    latest stage of evil!

--- best-program-in-the-world.py
+++ best-program-in-the-world.py
@@ -6,10 +6,12 @@ def best_program_in_the_world(arg, nums):
 # this are the changes i did (peer)
 
 nums = [8, 26, 11, 14, 21, 4, 26, 4, 12, 8, 11, 24]
-alpha = "abcdefghijklmnopqrstuvwxyz "
+alpha = "abcdefghijklmnopqrstuvwxyz !"
 
 best_program_in_the_world(alpha, nums)
 # this are the changes i did (peer)
 # todo: we will see
 
 # It's better now :D 
+#sdfkjjsdhfkhsdf
+#sdflkfjsdlfkjsdfl
